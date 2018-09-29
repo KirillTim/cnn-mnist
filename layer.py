Does not work because of wrong softmax implementation

layer.py
@@ -3,25 +3,35 @@ from scipy.special import expit
 from scipy.signal import convolve2d
 
 class NetworkLayer(object):
-    pass
+    def __init__(self, shape: tuple):
+        self.shape = shape
 
-class InitialLayer(NetworkLayer):
     def set_input(self, input):
-        self.output = input
+        self.prev.set_input(input)
+
+    def forward(self):
+        pass
 
     def backward(self, next_deriv: np.ndarray):
         pass
 
+class InitialLayer(NetworkLayer):
+    def __init__(self, shape):
+        super(InitialLayer, self).__init__(shape)
+    
+    def set_input(self, input):
+        self.output = input
+
 class FullLayer(NetworkLayer):
     def __init__(self, prev: NetworkLayer, size: tuple, learn_rate: float):
-        self.shape = size
-        self.neurons = np.zeros(prev.shape + size)
+        super(FullLayer, self).__init__(size)
+        self.neurons = np.random.uniform(size=prev.shape + size)
         self.prev = prev
         self.learn_rate = learn_rate
 
     def forward(self):
         self.prev.forward()
-        self.sumOutput = (self.prev.output * self.neurons).sum(axis=(0,1))
+        self.sumOutput = (self.prev.output.reshape(self.prev.shape + (1, 1)) * self.neurons).sum(axis=(0,1))
         self.output = expit(self.sumOutput)
 
     def backward(self, next_deriv: np.ndarray):
@@ -34,7 +44,7 @@ class FullLayer(NetworkLayer):
 
 class ConvolutionLayer(NetworkLayer):
     def __init__(self, prev: NetworkLayer, kernel: tuple, learn_rate: float):
-        self.shape = (prev.shape[0] - kernel[0] + 1, prev.shape[1] - kernel[1] + 1)
+        super(ConvolutionLayer, self).__init__((prev.shape[0] - kernel[0] + 1, prev.shape[1] - kernel[1] + 1))
         self.neurons = np.zeros(kernel)
         self.prev = prev
         self.learn_rate = learn_rate