6d0604ba345df88c4f64e30db1943576c64795ec
Dmitry Tomp <dmitrytomp@gmail.com>
2018-10-02 05:07:02 +0300

    Add convolution and subsampling; the network hasn't got much smarter

--- layer.py
+++ layer.py
@@ -12,7 +12,7 @@ class NetworkLayer(object):
     def forward(self):
         pass
 
-    def backward(self, next_deriv: np.ndarray):
+    def backward(self, next_deriv: np.ndarray, learn_rate: float):
         pass
 
 class InitialLayer(NetworkLayer):
@@ -23,14 +23,13 @@ class InitialLayer(NetworkLayer):
         self.output = input
 
 class FullLayer(NetworkLayer):
-    def __init__(self, prev: NetworkLayer, size: tuple, learn_rate: float, activation='sigmoid'):
+    def __init__(self, prev: NetworkLayer, size: tuple, activation='sigmoid'):
         super(FullLayer, self).__init__(size)
         #self.neurons = np.random.normal(size=prev.shape + size)
         self.neurons = np.random.uniform(low=-1, high=1, size=prev.shape + size)
         self.bias = np.random.uniform(low=-1, high=1, size=size)
         #self.neurons = np.zeros(prev.shape + size)
         self.prev = prev
-        self.learn_rate = learn_rate
         self.activation = activation
 
     def forward(self):
@@ -43,36 +42,71 @@ class FullLayer(NetworkLayer):
         else:
             raise ValueError("No such activation function: " + str(self.activation))
 
-    def backward(self, next_deriv: np.ndarray):
+    def backward(self, next_deriv: np.ndarray, learn_rate: float):
         if self.activation == 'sigmoid':
             sigmoid_diff = (1 / (np.exp(self.sumOutput) + np.exp(-self.sumOutput)) ** 2)
         elif self.activation == 'linear':
             sigmoid_diff = np.ones(self.sumOutput.shape)
         else:
             raise ValueError("No such activation function: " + str(self.activation))
-        self.bias -= self.learn_rate * sigmoid_diff * next_deriv
+        self.bias -= learn_rate * sigmoid_diff * next_deriv
         sigmoid_diff = sigmoid_diff.reshape((1,1) + sigmoid_diff.shape)
         prev_outputs = self.prev.output.reshape(self.prev.output.shape + (1,1))
         next_deriv = next_deriv.reshape((1, 1) + next_deriv.shape)
-        self.prev.backward((next_deriv * sigmoid_diff * self.neurons).sum(axis=(2,3)))
+        self.prev.backward((next_deriv * sigmoid_diff * self.neurons).sum(axis=(2,3)), learn_rate)
 
         minus = sigmoid_diff * prev_outputs * next_deriv
         #print(self.activation, minus.min(), minus.mean(), minus.max())
-        self.neurons -= self.learn_rate * minus
+        self.neurons -= learn_rate * minus
 
 class ConvolutionLayer(NetworkLayer):
-    def __init__(self, prev: NetworkLayer, kernel: tuple, learn_rate: float):
+    def __init__(self, prev: NetworkLayer, kernel: tuple):
         super(ConvolutionLayer, self).__init__((prev.shape[0] - kernel[0] + 1, prev.shape[1] - kernel[1] + 1))
-        self.neurons = np.zeros(kernel)
+        self.neurons = np.random.uniform(low=-1, high=1, size=kernel)
         self.prev = prev
-        self.learn_rate = learn_rate
 
     def forward(self):
         self.prev.forward()
         self.sumOutput = convolve2d(self.prev.output, self.neurons, mode="valid")
         self.output = expit(self.sumOutput)
 
-    def backward(self, next_deriv: np.ndarray):
+    def backward(self, next_deriv: np.ndarray, learn_rate: float):
         sigmoid_diff = (1 / (np.exp(self.sumOutput) + np.exp(-self.sumOutput)) ** 2)
-        self.prev.backward(convolve2d(next_deriv * sigmoid_diff, self.neurons[::-1, ::-1], mode="valid"))
-        self.neurons -= self.learn_rate * convolve2d(self.prev.output, next_deriv * sigmoid_diff, mode="valid")
+        self.prev.backward(convolve2d(next_deriv * sigmoid_diff, self.neurons[::-1, ::-1], mode="valid"), learn_rate)
+        self.neurons -= learn_rate * convolve2d(self.prev.output, next_deriv * sigmoid_diff, mode="valid")
+        
+class SubsampleLayer(NetworkLayer):
+    def __init__(self, prev: NetworkLayer, kernel: tuple):
+        assert prev.shape[0] % kernel[0] == 0 and prev.shape[1] % kernel[1] == 0
+        super(SubsampleLayer, self).__init__((prev.shape[0] // kernel[0], prev.shape[1] // kernel[1]))
+        self.prev = prev
+        self.kernel = kernel
+
+    def forward(self):
+        self.prev.forward()
+        self.output = self.prev.output.reshape((self.prev.shape[0] // self.kernel[0], self.kernel[0], self.prev.shape[1] // self.kernel[1], self.kernel[1])).mean(axis=(1, 3))
+
+    def backward(self, next_deriv: np.ndarray, learn_rate: float):
+        next_deriv = np.expand_dims(np.expand_dims(next_deriv, 1), 3)
+        next_deriv = np.concatenate([np.concatenate([next_deriv] * self.kernel[1], axis=3)] * self.kernel[0], axis=1).reshape(self.prev.shape)
+        self.prev.backward(next_deriv / (self.kernel[0] * self.kernel[1]), learn_rate)
+
+class CombineLayer(NetworkLayer):
+    def __init__(self, *args):
+        assert len(set(map(lambda a: a.shape[1], args))) == 1
+        super(CombineLayer, self).__init__((sum(map(lambda a: a.shape[0], args)), args[0].shape[1]))
+        self.prev = args
+
+    def set_input(self, input):
+        self.prev[0].set_input(input)
+
+    def forward(self):
+        for a in self.prev:
+            a.forward()
+        self.output = np.vstack(a.output for a in self.prev)
+
+    def backward(self, next_deriv: np.ndarray, learn_rate: float):
+        start = 0
+        for a in self.prev:
+            a.backward(next_deriv[start:start+a.shape[0], :], learn_rate)
+            start += a.shape[0]
--- test-mnist.py
+++ test-mnist.py
@@ -5,15 +5,29 @@ from random import shuffle
 mnist = fetch_mldata('MNIST original')
 
 baseLayer = InitialLayer((28, 28))
-full1 = FullLayer(baseLayer, (10, 10), 1e-2)
-result = FullLayer(full1, (1, 10), 1e-2, activation='linear')
+conv1 = ConvolutionLayer(baseLayer, (7, 7))
+conv2 = ConvolutionLayer(baseLayer, (7, 7))
+conv3 = ConvolutionLayer(baseLayer, (7, 7))
+conv4 = ConvolutionLayer(baseLayer, (7, 7))
+sub1 = SubsampleLayer(conv1, (2, 2))
+sub2 = SubsampleLayer(conv2, (2, 2))
+sub3 = SubsampleLayer(conv3, (2, 2))
+sub4 = SubsampleLayer(conv4, (2, 2))
+comb = CombineLayer(sub1, sub2, sub3, sub4)
+full1 = FullLayer(comb, (10, 10))
+result = FullLayer(full1, (1, 10), activation='linear')
 
 a = list(zip(mnist.data, mnist.target))
 shuffle(a)
 lossFunc = None
+prevLossFunc = None
+learnRate = 0.1
 for i, (data, target) in enumerate(a[:60000]):
     if i > 0 and i % 1000 == 0:
-        print(i, lossFunc)
+        print(i, lossFunc, learnRate)
+        if prevLossFunc is not None and lossFunc > prevLossFunc:
+            learnRate *= 0.5
+        prevLossFunc = lossFunc
     result.set_input(data.reshape((28, 28)) / 256)
     result.forward()
     exp = np.exp(result.output)
@@ -26,7 +40,7 @@ for i, (data, target) in enumerate(a[:60000]):
     cLoss = 10 * np.log(sumExp) - (label * result.output + (1 - label) * np.log(sumExpMinus)).sum()
     lossFunc = cLoss if lossFunc is None else lossFunc * 0.999 + cLoss * 0.001
     lossDeriv = 10 * probs - label - exp * sumSecond
-    result.backward(lossDeriv)
+    result.backward(lossDeriv, learnRate)
 
 lossFunc = 0
 nHits = 0
@@ -41,6 +55,3 @@ for data, target in a[60000:]:
     label = np.where(np.arange(0, 10) == int(target), 1, 0)
     lossFunc += 10 * np.log(sumExp) - (label * result.output + (1 - label) * np.log(sumExpMinus)).sum()
 print('Test loss', lossFunc / (len(a) - 60000), 'accuracy', nHits / (len(a) - 60000))
-
-#print(full1.neurons)
-#print(result.neurons)
